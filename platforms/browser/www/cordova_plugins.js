63b156bd830e61daea1e198a526b0960deaedfed
sidab <39728359+sidab@users.noreply.github.com>
2019-08-19 17:44:41 +0300

    fix

--- platforms/browser/www/cordova_plugins.js
+++ platforms/browser/www/cordova_plugins.js
@@ -19,9 +19,9 @@ module.exports.metadata =
 // TOP OF METADATA
 {
     "cordova-plugin-background-audio": "1.0.0",
-    "cordova-plugin-crosswalk-webview": "2.4.0",
     "cordova-plugin-splashscreen": "5.0.2",
-    "cordova-plugin-whitelist": "1.3.3"
+    "cordova-plugin-whitelist": "1.3.3",
+    "cordova-plugin-crosswalk-webview-v2": "2.4.3"
 }
 // BOTTOM OF METADATA
 });
