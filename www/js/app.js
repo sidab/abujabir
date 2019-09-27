24ed68fa4ade07afe297a4a15198cdc5bc0cb244
sidab <39728359+sidab@users.noreply.github.com>
2019-09-27 21:16:41 +0300

    fix

--- www/js/app.js
+++ www/js/app.js
@@ -681,3 +681,13 @@ audioElDom7.on('pause', function () {
   toolbarPlayer.find('.pause').addClass('display-none');
 
 });
+
+window.addEventListener("error", handleError, true);
+
+function handleError(evt) {
+  if (evt.message) { // Chrome sometimes provides this
+    alert("error: "+evt.message +" at linenumber: "+evt.lineno+" of file: "+evt.filename);
+  } else {
+    alert("error: "+evt.type+" from element: "+(evt.srcElement || evt.target));
+  }
+}
