14347205bc4d045160ffda4a7265671aebf76cec
sidab <39728359+sidab@users.noreply.github.com>
2019-10-15 19:21:03 +0300

    fix

--- www/js/app.js
+++ www/js/app.js
@@ -7,7 +7,7 @@ var app = new Framework7({
   root: '#app',
   name: 'Абу Джабир',
   theme: 'ios',
-  version: 5.3,
+  version: 5.6,
   routes: routes,
   backend: 'https://abujabir.ru/new/',
   touch: {
